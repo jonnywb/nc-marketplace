962695cf5fd2ea90e59323dccb092124963ebd17
Jonathan Brierley <jonnywb@users.noreply.github.com>
2023-12-09 21:11:35 +0000

    add files

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -4,45 +4,52 @@ import { getUserByUsername } from "../utils/utils";
 
 const Account = () => {
   const { user, setUser } = useContext(UserContext);
-  const [usernameInput, setUsernameInput] = useState('')
-  const [isError, setIsError] = useState(false)
+  const [usernameInput, setUsernameInput] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    getUserByUsername(usernameInput).then((user) => {
-      setUser(user)
-    })
-    .catch(() => {
-      setIsError(true)
-    })
-    setUsernameInput('')    
-  }
+    getUserByUsername(usernameInput)
+      .then((user) => {
+        setUser(user);
+      })
+      .catch(() => {
+        setIsError(true);
+      });
+    setUsernameInput("");
+  };
 
   const handleChange = (event) => {
-    setUsernameInput(event.target.value)
-  }
+    setUsernameInput(event.target.value);
+  };
 
   if (isError) {
-    return  (
-    <>
-      <p>Username not found</p>
-      <button onClick={() => { setIsError(false) }}>Take me back!</button>
-    </>
-    )
+    return (
+      <>
+        <p>Username not found</p>
+        <button
+          onClick={() => {
+            setIsError(false);
+          }}
+        >
+          Take me back!
+        </button>
+      </>
+    );
   }
 
   if (user.username) {
-    const { username, avatar_url, kudos, items_in_basket, items_ordered } = user
+    const { username, avatar_url, kudos, items_in_basket, items_ordered } = user;
     return (
       <>
         <h2>Account</h2>
         <h3>Hello {username}</h3>
-        <img style={{width: '75px', height: '75px'}} src={avatar_url} alt="your avatar" />
+        <img style={{ width: "75px", height: "75px" }} src={avatar_url} alt="your avatar" />
         <p>Kudos: {kudos}</p>
         <p>Your basket: {items_in_basket}</p>
         <p>Orders: {items_ordered}</p>
       </>
-    )
+    );
   }
 
   return (
@@ -53,7 +60,7 @@ const Account = () => {
           Username:
           <input name="username" value={usernameInput} onChange={handleChange} />
         </label>
-        <button>Log me in</button>
+        <button>Log in</button>
       </form>
     </section>
   );
